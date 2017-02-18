12915417f8ae3bf9d67c64f01438994e3e789161
PavelRoR <pavelvp2182@yandex.ru>
2017-02-18 15:00:57 +0500

    Добавлен 2 отзыв и переписан слайдер

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,24 @@
-$(".l_box").click(function() {
-    $(".layout").slideDown(900)
-});
-$("#close").click(function() {
-    $(".layout").slideUp(900)
+$(function() {
+    $(".l_box").click(function() {
+        $(".layout").slideDown(900)
+    });
+    $("#close").click(function() {
+        $(".layout").slideUp(900)
+    })
 });
 
-	$(function () {
-		$('#slides').slides({
-			preload: true,
-			preloadImage: 'img/loading.gif',
-			play: 5000,
-			pause: 2500,
-			hoverPause: true,
-			animationStart: function () {
-				$('.caption').animate({
-					bottom: -40
-				}, 100);
-			},
-			animationComplete: function (current) {
-				$('.caption').animate({
-					bottom: 0
-				}, 200);
-				if (window.console && console.log) {
-				// example return of current slide number
-				console.log(current);
-			};
-		}
-	});
-	});
+$(function() {
+    $("#else").click(function() {
+        $(".second_testim").slideDown(400);
+        $("#else").fadeOut(100);
+
+    })
+});
+
+$(function() {
+    $(".slides_container").slick({
+        autoplay: true,
+        dots: true
+    });
+    $(".slider-item").zoom()
+});
